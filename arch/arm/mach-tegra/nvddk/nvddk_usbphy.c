e460e560c4d09631a86aead2fd40aaa65067bbb2
vork <boss.benedikt@googlemail.com>
2012-06-29 17:46:24 +0200

    Me Genius. Stupid typo

--- arch/arm/mach-tegra/nvddk/nvddk_usbphy.c
+++ arch/arm/mach-tegra/nvddk/nvddk_usbphy.c
@@ -768,7 +768,7 @@ NvDdkUsbPhyPowerDown(
         /* Wait till Vbus is turned off */
         do
         {
-            NvOsWaitUS_interruptible(1000);
+            NvOsSleepUS_interruptible(1000);
             TimeOut -= 1000;
             e = hUsbPhy->Ioctl(hUsbPhy,
                     NvDdkUsbPhyIoctlType_VBusStatus,
